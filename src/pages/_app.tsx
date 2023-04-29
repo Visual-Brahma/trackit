a39ca63eda69a4b2cc1c3458303294984c216064
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2023-04-29 19:14:10 +0000

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Script src="//code.tidio.co/zyi8qkhaweop5tc4oenpsdstwnvcjw6j.js" async={true} />
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 }

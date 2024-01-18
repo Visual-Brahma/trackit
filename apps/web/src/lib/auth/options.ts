fac8c8044703339e85b62f84ddb61b3ee5d88dac
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-01-18 23:41:04 +0530

    fixed next-auth jwt bug (#8)

--- apps/web/src/lib/auth/options.ts
+++ apps/web/src/lib/auth/options.ts
@@ -1,12 +1,15 @@
-import { AuthOptions } from "next-auth"
+import { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { KyselyAdapter } from "@/lib/db/adapter"
 import { dbClient } from "@/lib/db/db_client"
 import EmailProvider from "next-auth/providers/email"
 import environmentVariables from "@/config/environment"
 
-export const authOptions: AuthOptions={
+export const authOptions: NextAuthOptions={
     adapter: KyselyAdapter(dbClient),
+    session: {
+        strategy: "jwt",
+    },
     providers: [
         EmailProvider({
             server: {

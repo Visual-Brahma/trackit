1d9c433f13f2ddd634bc5bd25e7c32437939c41f
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-03-01 19:03:56 +0000

    fixed redirects issue

---
+++ apps/web/src/app/(application)/mac/report-view/[slug]/route.ts
@@ -0,0 +1,20 @@
+import { dbClient } from "@/lib/db/db_client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const slug=req.nextUrl.pathname.split('/').pop();
+
+    if(!slug) return NextResponse.next();
+
+    const report=await dbClient.selectFrom("AttendanceReport")
+        .innerJoin("Meeting", "Meeting.id", "AttendanceReport.meetingId")
+        .select(["AttendanceReport.slug", "Meeting.groupId"])
+        .where("AttendanceReport.slug", "=", "mac_"+slug)
+        .executeTakeFirst();
+
+    if (report) {
+        return NextResponse.redirect(new URL(`/g/${report.groupId}/r/${report.slug}`, req.url))
+    } 
+
+    return NextResponse.next();
+}

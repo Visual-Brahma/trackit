dc0ad62d055798708a2af4efdfa435c684623ef0
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-03-01 18:41:10 +0000

    added redirects for old reports

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt';
 import environmentVariables from './config/environment';
+import { dbClient } from './lib/db/db_client';
 
 export async function middleware(req: NextRequest) {
     const res=NextResponse.next()
@@ -15,14 +16,34 @@ export async function middleware(req: NextRequest) {
         return NextResponse.redirect(new URL('/api/auth/signin?callback='+encodeURIComponent(req.nextUrl.pathname), req.url))
     }
 
-    // if user is signed in and the current path is / redirect the user to /account
+    // if user is signed in and the current path is / redirect the user to /dashboard
     if (session?.email&&req.nextUrl.pathname==='/') {
         return NextResponse.redirect(new URL('/dashboard', req.url))
+    }
+
+    if (req.nextUrl.pathname.startsWith('/mac/report-view/')||req.nextUrl.pathname.startsWith("/view-attendance-report/")) {
+        let slug=req.nextUrl.pathname.split('/').pop();
+
+        if (!slug) return res;
+
+        if (req.nextUrl.pathname.startsWith('/mac/report-view/')) {
+            slug="mac_"+slug;
+        }
+
+        const report=await dbClient.selectFrom("AttendanceReport")
+            .innerJoin("Meeting", "Meeting.id", "AttendanceReport.meetingId")
+            .select(["AttendanceReport.slug", "Meeting.groupId"])
+            .where("AttendanceReport.slug", "=", slug)
+            .executeTakeFirst();
+
+        if (report) {
+            return NextResponse.redirect(new URL(`/g/${report.groupId}/r/${report.slug}`, req.url))
+        }
     }
 
     return res
 }
 
 export const config={
-    matcher: ['/', '/dashboard/:path*', '/g/:path*'],
+    matcher: ['/', '/dashboard/:path*', '/g/:path*', '/mac/report-view/:path*', '/view-attendance-report/:path*'],
 }

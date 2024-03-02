99eee1fd46b2be11bdf79c96a81eda9a78d3473b
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-03-02 12:08:43 +0000

    minor fix

--- apps/web/src/app/(marketing)/mac/route.ts
+++ apps/web/src/app/(marketing)/mac/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
-    return NextResponse.redirect("/");
+export function GET(req: NextRequest) {
+    return NextResponse.redirect(new URL("/", req.url));
 }

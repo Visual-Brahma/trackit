06fa853f808d328b25982a8c5ee934f5d6afb1e0
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2024-05-06 11:43:37 +0000

    minor fix

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt';
 import environmentVariables from './config/environment';
-import { dbClient } from './lib/db/db_client';
+// import { dbClient } from './lib/db/db_client';
 
 export async function middleware(req: NextRequest) {
     const res=NextResponse.next()

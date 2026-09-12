836bf86566478ab4aa370827ef8f4902a18ae09e
Shubham Tiwari <72434294+blaze2004@users.noreply.github.com>
2026-09-12 22:51:47 +0530

    updated to new plunk api (#26)

--- apps/web/src/emails/index.tsx
+++ apps/web/src/emails/index.tsx
@@ -1,4 +1,4 @@
-import Plunk from "@plunk/node";
+import { Plunk } from "plunk-node-sdk";
 import environmentVariables from "@/config/environment";
 
 export const plunk = new Plunk(environmentVariables.email.plunkApiKey);
